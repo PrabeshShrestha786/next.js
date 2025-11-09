bf4760a2cb50953826b134b4606690367d66621c
Prabesh786 <e2301430@vamk.fi>
2025-11-09 22:20:58 +0200

    chapter 3 task 1

---
+++ 3-styling-in-next-js/assignment1/app/components/Title.js
@@ -0,0 +1,6 @@
+// components/Title.js
+import styles from './Title.module.css';
+
+export default function Title() {
+  return <h1 className={styles.title}>Welcome to My Styled Title</h1>;
+}
---
+++ 3-styling-in-next-js/assignment1/app/page.tsx
@@ -0,0 +1,12 @@
+import Title from "./components/Title";
+
+export default function HomePage() {
+  return (
+    <main>
+      <Title />
+      <p style={{ textAlign: 'center' }}>
+        This is a sample app showing how to use CSS Modules in Next.js.
+      </p>
+    </main>
+  );
+}
