81c6fd1459106dc2f6c18ba9fef0074731391c40
Prabesh786 <e2301430@vamk.fi>
2025-11-09 21:34:17 +0200

    chapter 1 task 4

---
+++ 2-next-js-basics/assignment4/app/page.tsx
@@ -0,0 +1,16 @@
+
+
+export default function HomePage() {
+  return (
+    <main style={{ textAlign: "center", marginTop: "60px" }}>
+      <h1>Welcome to My Awesome App 🚀</h1>
+      <p>
+        This is a simple Next.js project built with the App Router. 
+        Explore the site, learn about how it works, and navigate seamlessly 
+        using Next.js client-side routing.
+      </p>
+
+      
+    </main>
+  );
+}
