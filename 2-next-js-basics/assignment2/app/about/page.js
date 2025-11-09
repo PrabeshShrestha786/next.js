72a338847ffe7be144a0d229c047af1f91c6aa90
Prabesh786 <e2301430@vamk.fi>
2025-11-09 21:01:19 +0200

    chapter 2 task 2

---
+++ 2-next-js-basics/assignment2/app/about/page.js
@@ -0,0 +1,8 @@
+export default function AboutPage() {
+  return (
+    <div>
+      <h1 style={{ fontSize: '2rem', fontWeight: 'bold' }}>About this course</h1>
+      <p>This course teaches how to build modern web applications using Next.js, React, and other powerful web technologies.</p>
+    </div>
+  );
+}
